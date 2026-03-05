79266b127ea6cb8ffb49f8971ce24d85d9dbf1af
Morten V. Pedersen <morten@mortenvp.com>
2026-03-05 14:48:11 +0100

    set Bitwarden SSH_AUTH_SOCK and add less package

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,9 +2,9 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-set -Ux SSH_AUTH_SOCK $XDG_RUNTIME_DIR/gnupg/S.gpg-agent.ssh
+set -gx SSH_AUTH_SOCK "$HOME/.bitwarden-ssh-agent.sock"
 alias lg="lazygit"
 alias ll="ls -la"
+alias c="code . -n"
 
 zoxide init fish | source
-set -x SSH_AUTH_SOCK /home/mvp/.bitwarden-ssh-agent.sock
