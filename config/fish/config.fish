bb33cebec32ed9b04545199229cbc09319604b13
Morten V. Pedersen <morten@mortenvp.com>
2026-03-05 10:02:06 +0100

    add fish ll alias and github cli package

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -4,6 +4,7 @@ end
 
 set -Ux SSH_AUTH_SOCK $XDG_RUNTIME_DIR/gnupg/S.gpg-agent.ssh
 alias lg="lazygit"
+alias ll="ls -la"
 
 zoxide init fish | source
 set -x SSH_AUTH_SOCK /home/mvp/.bitwarden-ssh-agent.sock
